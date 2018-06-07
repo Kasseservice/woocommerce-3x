252b9cb19fd59995d7bafae95cb477f5d662caa3
kgpanteon <kash@panteon.no>
2018-06-07 14:29:28 +0530

    Setting link added at pludin list page

--- wp-content/plugins/duellintegration/duellintegration.php
+++ wp-content/plugins/duellintegration/duellintegration.php
@@ -17,6 +17,8 @@ class Duellintegration {
     public function __construct() {
         register_activation_hook(__FILE__, array($this, 'setup_install'));
         register_deactivation_hook(__FILE__, array($this, 'setup_uninstall'));
+        // Add setting links to plugin page
+        add_filter("plugin_action_links", array($this, 'plugin_add_settings_link'), 10, 5);
         //==pre intialize values
         add_action('plugins_loaded', array($this, 'plugin_init_setup'));
         // Hook into the admin menu
@@ -81,6 +83,16 @@ class Duellintegration {
             //wp_schedule_single_event($next3am, 'duell_cron_sync_orders');
             wp_schedule_event(strtotime('03:00:00'), 'daily3am', 'duell_cron_sync_orders');
         }
+    }
+
+    /*
+     * Add plugin setting page link in plugin page
+     */
+
+    function plugin_add_settings_link($links) {
+        $settings_link = '<a href="admin.php?page=duell-settings">' . __('Settings') . '</a>';
+        array_push($links, $settings_link);
+        return $links;
     }
 
     /*
@@ -212,7 +224,7 @@ class Duellintegration {
               <?php wp_nonce_field('duellintegration_nonce', 'duellintegration_nonce_field'); ?>
               <?php settings_fields('duellintegration'); ?>
               <?php do_settings_sections('duellintegration'); ?>
-              <?php submit_button(); ?>
+        <?php submit_button(); ?>
             </form>
           </div>
           <div class="col-right borderL">
@@ -259,48 +271,48 @@ class Duellintegration {
 
     public function setup_action_javascript() {
         ?><script>
-                    (function ($) {
-                      function blockUI()
-                      {
-                        jQuery("#blocker").css('display', "");
-                      }
-                      function unblockUI()
-                      {
-                        jQuery("#blocker").css('display', "none");
-                      }
-                      var inProcess = false;
-                      var $output = $('#manual-cron-output');
-                      $('.manual-cron').click(function () {
-                        if (inProcess == false) {
-                          inProcess = true;
-                          console.log($(this).attr('data-type'))
-                          jQuery.ajax({
-                            type: "POST",
-                            url: ajaxurl,
-                            data: {action: 'manual_run_cron_action', param: $(this).attr('data-type')},
-                            cache: false,
-                            beforeSend: function () {
-                              // jQuery('#button-syncmanually').button('loading');
-                              blockUI();
-                            },
-                            complete: function () {
-                              //jQuery('#button-syncmanually').button('reset');
-                              unblockUI();
-                              inProcess = false;
-                            },
-                            success: function (data) {
-                              $output.html(data.response);
-                            },
-                            error: function (jqXHR, textStatus, errorThrown) {
-                              $output.html('<code>ERROR</code> ' + textStatus + ' ' + errorThrown);
-                            }
-                          }).done(function (msg) {
-                            // alert("Data Saved: " + msg.response);
-                            $output.html('<code>OK</code>' + msg.response);
-                          });
-                        }
-                      });
-                    }(jQuery));
+            (function ($) {
+              function blockUI()
+              {
+                jQuery("#blocker").css('display', "");
+              }
+              function unblockUI()
+              {
+                jQuery("#blocker").css('display', "none");
+              }
+              var inProcess = false;
+              var $output = $('#manual-cron-output');
+              $('.manual-cron').click(function () {
+                if (inProcess == false) {
+                  inProcess = true;
+                  console.log($(this).attr('data-type'))
+                  jQuery.ajax({
+                    type: "POST",
+                    url: ajaxurl,
+                    data: {action: 'manual_run_cron_action', param: $(this).attr('data-type')},
+                    cache: false,
+                    beforeSend: function () {
+                      // jQuery('#button-syncmanually').button('loading');
+                      blockUI();
+                    },
+                    complete: function () {
+                      //jQuery('#button-syncmanually').button('reset');
+                      unblockUI();
+                      inProcess = false;
+                    },
+                    success: function (data) {
+                      $output.html(data.response);
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+                      $output.html('<code>ERROR</code> ' + textStatus + ' ' + errorThrown);
+                    }
+                  }).done(function (msg) {
+                    // alert("Data Saved: " + msg.response);
+                    $output.html('<code>OK</code>' + msg.response);
+                  });
+                }
+              });
+            }(jQuery));
         </script>
         <?php
     }
